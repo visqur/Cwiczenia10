a90df32e6abe0f909f60cb7dec009abf4a4e28d3
Bartek <bartek.stanski01@gmail.com>
2025-01-04 00:09:37 +0100

    Cwiczenia10

--- src/Cwiczenia10/DataManagment.java
+++ src/Cwiczenia10/DataManagment.java
@@ -1,3 +1,5 @@
+package Cwiczenia10;
+
 import java.io.*;
 import java.util.Scanner;
 
--- src/Cwiczenia10/FileHandlingTask1.java
+++ src/Cwiczenia10/FileHandlingTask1.java
@@ -1,3 +1,5 @@
+package Cwiczenia10;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
